b0dfe0e66ba38181671a96427e9b540592dfb1b0
bharri51 <66283407+bharri51@users.noreply.github.com>
2020-07-20 17:09:31 -0400

    Excel seems like the best way to go

--- SalesReporting_xlsximport.py
+++ SalesReporting_xlsximport.py
@@ -1,21 +1,25 @@
 import csv
-import xlsx
 import pandas as pd
 import os
 import time
 from datetime import datetime
 import numpy
 from time import mktime
+import json
+from openpyxl import load_workbook
+import openpyxl
+import dateutil
+
 
 
 sales_data_excel = pd.ExcelFile('Demo Sales Data.xlsx')
-  
-# View the excel_file's sheet names 
-print(sales_data_excel.sheet_names) 
-  
-# Load the excel_file's Sheet1 as a dataframe 
-sheet1 = excel_file.parse('Sheet1') 
-print(sheet1) 
+sales_data = sales_data_excel.parse('Demo Sales Data', header=None)
+sales_data.columns = ['Salesperson','Date','Revenue','Cost']
+sales_data['Profit'] = sales_data['Revenue'] - sales_data['Cost']
+sales_data['Margin'] = ((sales_data['Revenue'] - sales_data['Cost']) / sales_data['Revenue']) * 100
+sales_data['Commission'] = sales_data['Profit'] * .25
+print(sales_data)
+
 
 
 
